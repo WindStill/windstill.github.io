bf434170541cc893d739c9d33c5c2eb66900aa8c
Li Yuan <202070317@163.com>
2013-12-26 22:10:00 +0800

    post

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/blog/"
-http_images_path = "/blog/images"
+http_path = "/myblog/"
+http_images_path = "/myblog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/blog/fonts"
-css_dir = "public/blog/stylesheets"
+http_fonts_path = "/myblog/fonts"
+css_dir = "public/myblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
